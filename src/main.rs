4abcc57aa8de6cef062a09906aac1587b456e0b2
itsmenewbie03 <2101102516@student.buksu.edu.ph>
2024-10-17 14:25:33 +0800

    feat: use iterators because why not?

--- src/main.rs
+++ src/main.rs
@@ -1,115 +1,122 @@
 use fancy_regex::Regex;
-use std::fs::File;
-use std::io::{self, BufRead};
-use std::process::Command;
-use std::thread;
-use std::time::Duration;
+use std::{path::PathBuf, process::Command, str::FromStr, thread, time::Duration};
 
-fn print_txt(input: &str) {
-    if let Ok(output) = Command::new("figlet")
-        .args(["-w", "150"])
-        .arg(input)
-        .output()
-    {
-        if output.status.success() {
-            println!("{}", String::from_utf8_lossy(&output.stdout));
+#[derive(Debug)]
+struct Line {
+    line: String,
+    time: String,
+}
+
+// NOTE: I'm pretending to understand this lifetime shit xD
+// OK that's it, I'm still now worthy to use References xD
+#[derive(Debug)]
+struct Lyrics {
+    lines: Vec<Line>,
+}
+
+impl Line {
+    fn diff(&self, next: &Line) -> Duration {
+        let cur = self.to_ms();
+        let next = next.to_ms();
+        Duration::from_secs_f64(next - cur)
+    }
+
+    fn to_ms(&self) -> f64 {
+        // INFO: format 00:50.00
+        let mut parts = self.time.split(&[':']);
+        let mins = parts.next().expect("Minutes part not found");
+        let mins = mins
+            .parse::<f64>()
+            .unwrap_or_else(|_| panic!("Failed to parse minutes to float.\nMins: {}", mins))
+            * 60_f64;
+        let sec = parts.next().expect("Seconds part not found");
+        let sec = sec
+            .parse::<f64>()
+            .expect("Failed to parse seconds to float.");
+        mins + sec
+    }
+}
+
+impl Lyrics {
+    fn iter(&self) -> LyricsIterator {
+        LyricsIterator {
+            lyrics: self,
+            index: 0,
+        }
+    }
+    fn from_file(path: std::path::PathBuf) -> Result<Self, String> {
+        let content = std::fs::read_to_string(path);
+        match content {
+            // NOTE: valid line format
+            // [00:50.00] @itsmenewbie03
+            Ok(lyrics) => {
+                let mut lines: Vec<Line> = lyrics
+                    .trim()
+                    .split("\n")
+                    .map(|line| {
+                        let re = Regex::new(r"((?<=\[)[0-9:\.]+(?=\]))(\])(.*)").unwrap();
+                        let matches = re.captures(line).unwrap().unwrap();
+                        let time = matches.get(1).expect("Time is not found!").as_str();
+                        let line = matches.get(3).expect("Line is not found!").as_str();
+                        Line {
+                            time: time.to_owned(),
+                            line: line.to_owned(),
+                        }
+                    })
+                    .collect();
+                Ok(Lyrics { lines })
+            }
+            Err(err) => Err(format!("Failed to read lyric file!\nERR: {}", err)),
+        }
+    }
+}
+
+struct LyricsIterator<'a> {
+    lyrics: &'a Lyrics,
+    index: usize,
+}
+
+impl<'a> Iterator for LyricsIterator<'a> {
+    type Item = &'a Line;
+
+    fn next(&mut self) -> Option<Self::Item> {
+        if self.index < self.lyrics.lines.len() {
+            let item = &self.lyrics.lines[self.index];
+            self.index += 1;
+            Some(item)
         } else {
-            eprintln!(
-                "Figlet command failed:\n{}",
-                String::from_utf8_lossy(&output.stderr)
-            );
-        }
-    } else {
-        eprintln!("Error running figlet.");
-    }
-}
-
-fn remove_timestamp(line: &str) -> String {
-    if let Some(start) = line.find('[') {
-        if let Some(end) = line[start..].find(']') {
-            let mut result = String::with_capacity(line.len() - (end + 2));
-            result.push_str(&line[..start]);
-            result.push_str(&line[start + end + 1..]);
-
-            return result;
+            None
         }
     }
-
-    line.to_string()
 }
 
-fn get_timestamp(line: &str) -> &str {
-    if let Some(start) = line.find('[') {
-        if let Some(end) = line[start..].find(']') {
-            return &line[start + 1..start + end];
-        }
-    }
-
-    ""
-}
-
-fn pause_between_timestamps(timestamp1: &str, timestamp2: &str) {
-    let pattern = Regex::new(r"(?:(\d+):)?(\d+\.\d+)").unwrap();
-    let captures1 = pattern.captures(timestamp1).unwrap().unwrap();
-    let captures2 = pattern.captures(timestamp2).unwrap().unwrap();
-
-    let minutes1 = captures1
-        .get(1)
-        .map_or(0, |m| m.as_str().parse::<u64>().unwrap());
-    let seconds1 = captures1
-        .get(2)
-        .map_or(0.0, |m| m.as_str().parse::<f64>().unwrap());
-
-    let minutes2 = captures2
-        .get(1)
-        .map_or(0, |m| m.as_str().parse::<u64>().unwrap());
-    let seconds2 = captures2
-        .get(2)
-        .map_or(0.0, |m| m.as_str().parse::<f64>().unwrap());
-
-    let total_seconds1 = (minutes1 * 60) as f64 + seconds1;
-    let total_seconds2 = (minutes2 * 60) as f64 + seconds2;
-
-    let time_to_pause = (total_seconds2 - total_seconds1).abs();
-    std::thread::sleep(Duration::from_secs_f64(time_to_pause));
-}
-
-fn main() -> io::Result<()> {
-    let file = File::open("res/lyrics.txt")?;
-    let lines: Vec<_> = io::BufReader::new(file).lines().collect();
-
-    let line_count = lines.len();
-
+fn clear() {
     Command::new("clear")
         .status()
         .expect("failed to clear screen");
+}
 
-    let t1 = thread::spawn(|| {
+fn play() -> std::thread::JoinHandle<()> {
+    thread::spawn(|| {
         Command::new("ffplay")
             .args(["-v", "0", "-nodisp", "-autoexit", "res/music.mp3"])
             .status()
             .expect("failed to execute ffplay");
-    });
-
-    for x in 0..line_count {
-        Command::new("clear")
-            .status()
-            .expect("failed to clear screen");
-
-        let ts_next = if x != line_count - 1 {
-            get_timestamp(lines[x + 1].as_ref().unwrap())
-        } else {
-            "00:00.00"
-        };
-
-        let line_now = &lines[x];
-        let line_now_cleaned = remove_timestamp(line_now.as_ref().unwrap().as_str());
-        let ts_now = get_timestamp(line_now.as_ref().unwrap().as_str());
-        print_txt(&line_now_cleaned);
-        pause_between_timestamps(ts_now, ts_next);
+    })
+}
+fn main() {
+    let lyrics = Lyrics::from_file(PathBuf::from_str("res/lyrics.txt").unwrap())
+        .expect("Failed to parse lyrics file.");
+    clear();
+    let play_handle = play();
+    for line in lyrics.iter().collect::<Vec<_>>().windows(2) {
+        let cur = line.first().expect("100% RUST BUG!");
+        let next = line.last().expect("100% RUST BUG!");
+        let diff = cur.diff(next);
+        println!("{}", cur.line);
+        thread::sleep(diff);
+        clear();
     }
-
-    t1.join().expect("thread panicked");
-
-    Ok(())
+    println!("{}", lyrics.lines.last().expect("100% RUST BUG!").line);
+    play_handle.join().unwrap();
 }
